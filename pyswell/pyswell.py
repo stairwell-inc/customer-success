d2560e8bee698730696174622779a548a3080dfa
mikewilusz-stairwell <90697468+mikewilusz-stairwell@users.noreply.github.com>
2023-03-15 19:58:23 +0000

    Update pyswell.py with argparse

--- pyswell/pyswell.py
+++ pyswell/pyswell.py
@@ -14,29 +14,19 @@ import requests    # Calling REST API
 from requests.exceptions import ConnectTimeout
 import sys         # Reading CLI arguments
 import hashlib     # Calculating SHA256
+import argparse    # CLI argument handling
 
-# Help screen if no args
-if (len(sys.argv)) == 1:
-   print("pyswell.py - CLI Interface for Stairwell Inception")
-   print("--------------------------------------------------")
-   print("Commands:")
-   print("mkasset    - Create a new asset")
-   print("uploadpath - Recursively upload a path (requires ASSET_ID set in script)")
-   print("")
-   exit(0)
-   
-if (sys.argv[1] == "mkasset" and len(sys.argv) < 5):
-   print("Command: pyswell.py mkasset <ASSET NAME> <ENVIRONMENT ID> <API KEY>")
-   
+
+ 
 # Create asset
-if (sys.argv[1] == "mkasset" and len(sys.argv) == 5):
+def make_asset(args, asset_id):
    url = "https://app.stairwell.com/v202112/assets"
    payload = {
-    "label": sys.argv[2],
-    "environment_id": {"id": sys.argv[3]}
+    "label": args.name,
+    "environment_id": {"id": args.env_id}
    }
    headers = {
-    "Authorization": sys.argv[4],
+    "Authorization": args.api_key,
     "Content-Type": "application/json"
    }
    response = requests.request("POST", url, json=payload, headers=headers)
@@ -47,15 +37,6 @@ if (sys.argv[1] == "mkasset" and len(sys.argv) == 5):
    else:
       print("Error: " + response.text)
       exit(0)
-
-# Request onboarding if ASSET_ID is not set
-if ASSET_ID == "":
-   print("ASSET_ID not set in pySwell. Run 'pyswell.py mkasset <ASSET NAME> <ENVIRONMENT ID> <API KEY>' and add ASSET_ID to script.")
-   exit(0)
-   
-if (sys.argv[1] == "uploadpath" and len(sys.argv) == 2):
-    print("Command: pyswell.py uploadpath <BASE DIRECTORY>")
-    exit(0)
 
 def sendToStairwell(file):
    file_to_upload = open(file, "rb")
@@ -105,9 +86,11 @@ def sendToStairwell(file):
          return
     
 # Upload path
-if (sys.argv[1] == "uploadpath" and len(sys.argv) == 3):
-   print("Uploading recursively from path: " + sys.argv[2])
-   for root, dirs, files in os.walk(sys.argv[2], topdown=True):
+def upload(args, ASSET_ID):
+   if ASSET_ID == "":
+      sys.exit("The ASSET_ID variable is not set, exiting...")
+   print("Uploading recursively from path: " + args.path)
+   for root, dirs, files in os.walk(args.path, topdown=True):
       for name in files:
          print(os.path.join(root, name))
          filePath = os.path.join(root, name)
@@ -137,3 +120,39 @@ if (sys.argv[1] == "uploadpath" and len(sys.argv) == 3):
          # Not interesting
          else:
             print("Not interesting")
+            
+def main():
+   # create the top-level parser
+   parser = argparse.ArgumentParser(prog='pyswell.py')
+   subparsers = parser.add_subparsers(help='available commands')
+
+   # By default, sub-parseres are mutually exclusive
+   # Create parser for mkasset
+   parser_mkasset = subparsers.add_parser('mkasset')
+   parser_mkasset.add_argument('--name',
+                                required=True,
+                                help="Name of the asset to create")
+   parser_mkasset.add_argument('--env_id',
+                                required=True,
+                                help="Environment ID to create the asset in")
+   parser_mkasset.add_argument('--api_key',
+                                required=True,
+                                help="API key to create the asset with")
+   parser_mkasset.set_defaults(func=make_asset)
+
+   # Create parser for upload command
+   parser_upload = subparsers.add_parser('upload')
+   parser_upload.add_argument('--path',
+                                   required=True,
+                                   help="Path to the directory to upload")
+   parser_upload.set_defaults(func=upload)
+
+   # If no arguments have been passed, print help
+   if len(sys.argv) == 1:
+      parser.print_help()
+
+   args = parser.parse_args()
+   args.func(args, ASSET_ID)
+            
+if __name__ == "__main__":
+   main()
